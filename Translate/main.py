bb7d823d42efeb745de234b8142b199d655e7d26
demonstalker show <demostalker26@gmail.com>
2021-07-03 11:51:26 +0300

    Полностью переделано с графическим интерфейсом!

--- Translate/main.py
+++ Translate/main.py
@@ -1,4 +1,7 @@
 import requests
+from tkinter import *
+from tkinter.ttk import *
+from functools import partial
 
 URL_AUTH = 'https://developers.lingvolive.com/api/v1.1/authenticate'
 URL_TRANSLATE = 'https://developers.lingvolive.com/api/v1/Minicard'
@@ -7,31 +10,46 @@ KEY = 'ZjJmYTY3ZTQtZjA3OS00MzljLWJhODctYTkzNmIxZGZjNjdiOjBiNmYwM2VjMGY5YzRhZTZiZ
 headers_auth = {'Authorization': 'Basic ' + KEY}
 auth = requests.post(URL_AUTH, headers=headers_auth)
 
-if auth.status_code == 200:
+def Translate(headers_auth, auth):
+    auth.status_code =200         
     token = auth.text
-    while True:
-        a = input('Хотите перевести слово с английского на русский, иначе будет обратно? [yes/нет]: ')
-        if a == 'yes':
-            word = input('Введите слово для перевода: \n')        
-            if word:
-                headers_translate = {'Authorization': 'Bearer ' + token}
-                params = {'text': word, 'srcLang': 1033,  'dstLang': 1049}           
-                r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
-                res = r.json()
-                try:
-                   print(res['Translation']['Translation'])
-                except:
-                   print('Не найдено варианта для перевода')        
-        elif a == "нет":
-               word = input('Введите слово для перевода: \n')        
-               if word:
-                  headers_translate = {'Authorization': 'Bearer ' + token}
-                  params = {'text': word, 'srcLang': 1049,  'dstLang': 1033}           
-                  r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
-                  res = r.json()
-                  try:
-                     print(res['Translation']['Translation'])
-                  except:
-                     print('Не найдено варианта для перевода')            
-else:
-    print('Error!')
+    word = txt.get()    
+    if var.get() == 0:
+        headers_translate = {'Authorization': 'Bearer ' + token}
+        params = {'text': word, 'srcLang': 1033,  'dstLang': 1049}           
+        r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
+        res = r.json()
+        label.configure(text=res['Translation']['Translation'])            
+        try:
+            print(res['Translation']['Translation'])
+        except:
+            print('Не найдено варианта для перевода')    
+    elif var.get() == 1:
+        headers_translate = {'Authorization': 'Bearer ' + token}
+        params = {'text': word, 'srcLang': 1049,  'dstLang': 1033}           
+        r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
+        res = r.json()
+        label.configure(text=res['Translation']['Translation'])
+        try:
+            print(res['Translation']['Translation'])
+        except:
+            print('Не найдено варианта для перевода')
+
+
+window = Tk()
+window.title("demonstalkerTranslate")
+window.geometry("800x600")
+var = IntVar()
+var.set(0)
+btn_translate_er = Radiobutton(window, text='С английского на русский',variable=var, value=0)
+btn_translate_er.grid(column=0, row=0)
+btn_translate_re = Radiobutton(window, text='С русского на английский',variable=var, value=1)
+btn_translate_re.grid(column=2, row=0)
+
+txt = Entry(window,width=20)
+txt.grid(column=4,row=0)
+btn_translate = Button(window, text="Перевести",command=partial(Translate,headers_auth, auth))
+btn_translate.grid(column=5, row=0)
+label = Label(window, text="Здесь будет перевод",font=("Comic Sans MS",24, "bold"))
+label.grid(column=0,row=1)
+window.mainloop() 
