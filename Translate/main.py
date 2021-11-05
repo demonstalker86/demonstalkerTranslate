6fc8f0138dcdac4e2c5444d4c75519e350f90b1c
demonstalker show <demostalker26@gmail.com>
2021-11-05 13:26:52 +0300

    New translator based on Google

--- Translate/main.py
+++ Translate/main.py
@@ -3,6 +3,9 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import *
 from functools import partial
+from tkinter import scrolledtext
+
+
 
 URL_AUTH = 'https://developers.lingvolive.com/api/v1.1/authenticate'
 URL_TRANSLATE = 'https://developers.lingvolive.com/api/v1/Minicard'
@@ -15,25 +18,26 @@ auth = requests.post(URL_AUTH, headers=headers_auth)
 def Translate(headers_auth, auth):
     auth.status_code =200         
     token = auth.text
-    word = txt.get()    
+    word = txt.get("1.0", 'end')    
     if var.get() == 0:
         headers_translate = {'Authorization': 'Bearer ' + token}
-        params = {'text': word, 'srcLang': 1033,  'dstLang': 1049}           
+        params = {'prefix': word, 'srcLang': 1033,  'dstLang': 1049, 'pageSize': 20, 'StartPos': ''}           
         r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
         res = r.json()                    
         try:
-            label.configure(text=res['Translation']['Translation'])
-        except:
-            label.configure(text='Не найдено варианта для первеода')   
+              label.configure(text = res["Translation"]["Translation"])
+        except RuntimeError:
+              label.configure(text='Не найдено варианта для перевода')            
     elif var.get() == 1:
         headers_translate = {'Authorization': 'Bearer ' + token}
-        params = {'text': word, 'srcLang': 1049,  'dstLang': 1033}           
+        params = {'prefix': word, 'srcLang': 1049,  'dstLang': 1033, 'pageSize': 20, 'StartPos': ''}           
         r = requests.get(URL_TRANSLATE, headers=headers_translate, params=params)
         res = r.json()       
         try:
-            label.configure(text=res['Translation']['Translation'])
-        except:
-            label.configure(text='Не найдено варианта для первеода')
+            label.configure(text = res["Translation"]["Translation"])
+        except RuntimeError:
+            label.configure(text='Не найдено варианта для перевода')
+            
 
 window = Tk()
 window.title("demonstalkerTranslate")
@@ -44,7 +48,8 @@ btn_translate_er.grid(column=0, row=0)
 btn_translate_re = Radiobutton(window, text='С русского на английский',variable=var, value=1)
 btn_translate_re.grid(column=1, row=0)
 
-txt = Entry(window,width=20)
+txt = scrolledtext.ScrolledText(window,width=30,height=10)
+txt.insert(INSERT, 'Текстовое поле')
 txt.grid(column=2,row=0)
 ttk.Style().configure("TButton", font=("Helvetica",12, "bold"), padding=6, relief="flat", background="blue", foreground="red")
 btn_translate = Button(window, text="Перевести", command=partial(Translate,headers_auth, auth))
---
+++ Translate/module1.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
+from googletrans import Translator
+
+
+
+def translater():
+    text = txt_input.get('1.0', END)
+    a = translator.translate(text, dest='ru')   
+    txt_translate.delete('1.0', END)
+    txt_translate.insert('1.0', a.text)
+
+
+
+
+window = Tk()
+window.geometry("600x400")
+window.title("demonstalkerTranslate")
+window.resizable(width=False, height=False)
+window['bg'] = 'black'
+translator = Translator()
+
+ttk.Style().configure("TLabel", foreground="white", background="black")
+label = Label(window,  text="Введите текст на английском", font=("Comic Sans MS",15, "bold"))
+label.place(relx=0.5, y=30, anchor=CENTER)
+txt_input = Text(window, width=45, height=5, font='Arial 12 bold')
+txt_input.place(relx=0.5, y=100, anchor=CENTER)
+
+ttk.Style().configure("TButton", font=("Helvetica",12, "bold"), padding=6, relief="flat", background="blue", foreground="red")
+btn_translate = Button(window, width=45,text="Перевести", command=translater)
+btn_translate.place(relx=0.5, y=180, anchor=CENTER)
+
+txt_translate = Text(window, width=45, height=5, font='Arial 12 bold')
+txt_translate.place(relx=0.5, y=260, anchor=CENTER)
+
+
+window.mainloop() 
